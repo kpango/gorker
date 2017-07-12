e4562bc568a09d04af21b5501d677129eafacd1b
yusukato <yusukato@yahoo-corp.jp>
2017-07-13 07:27:25 +0900

    fix

--- example/sample.go
+++ example/sample.go
@@ -15,15 +15,12 @@ func main() {
 	for i := 0; i < 10000; i++ {
 		func(n int) {
 			dispatcher.Add(func() error {
-				workers := runtime.NumGoroutine() - 2 // main and worker observer routine
-				fmt.Printf("%03d:\t workers: %d\t%v\n",
-					n, workers, time.Now().Format(time.RFC3339))
+				fmt.Printf("%03d:\t workers: %d\t%v\n", n, runtime.NumGoroutine()-2, time.Now().Format(time.RFC3339))
 				time.Sleep(time.Millisecond * 100)
 				return nil
 			})
 		}(i)
 	}
-
 	dispatcher.Start()
 
 	time.Sleep(time.Second * 5)
@@ -40,6 +37,12 @@ func main() {
 
 	gorker.UpScale(20)
 	time.Sleep(time.Second * 5)
+
+	dispatcher.Add(func() error {
+		fmt.Printf("last worker:\t workers: %d\t%v\n", runtime.NumGoroutine()-2, time.Now().Format(time.RFC3339))
+		time.Sleep(time.Millisecond * 100)
+		return nil
+	})
 
 	gorker.UpScale(200)
 	time.Sleep(time.Second * 5)
